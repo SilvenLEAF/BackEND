648fa082f4fe1aea96c8147e48cd5191abefbe98
SilvenLEAF <SilvenLEAF@gmail.com>
2020-08-19 12:32:39 +0530

    added credentials on GoogleStrategy. I mean added the strategy options.

--- OAuth/config/passportSetup.js
+++ OAuth/config/passportSetup.js
@@ -8,6 +8,9 @@ const GoogleStrategy = require('passport-google-oauth20');
 passport.use(
   new GoogleStrategy({
     // options for strategy
+    callbackURL: '/auth/google/redirect',
+    clientID: '948666598374-tj740dlbnbt9dmtvi6k1lifi16ml13mj.apps.googleusercontent.com',
+    clientSecret: '7kl5vertxRo6C8ezBT0YLqp-'
   }),
   (accessToken, refreshToken, profile, done)=>{
     // passport callback function
