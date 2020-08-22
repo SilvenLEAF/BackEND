576d43e28143ac695c3867866ebe0c42f18641c7
SilvenLEAF <SilvenLEAF@gmail.com>
2020-08-22 15:50:27 +0530

    added facebook strategy, can go to facebook sign in page

--- OAUTH-Total/app.js
+++ OAUTH-Total/app.js
@@ -37,3 +37,34 @@ passport.use( new FacebookStrategy({
     user = { ...profile };
     return cb(null, profile)
   }))
+
+
+
+
+  const app = express();
+  app.use(passport.initialize());
+
+
+
+
+
+/* -------------------------------------
+.                 routes
+------------------------------------- */
+app.get('/auth/facebook', passport.authenticate('facebook') )
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook'),
+  (req, res)=>{
+    res.redirect('/profile');
+  }
+)
+app.get('/profile', (req, res)=>{
+  res.json(`Hi. This is your profile`);
+})
+
+
+// -------------------------------LISTEN
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=>{
+  console.log(`Server is running on port ${PORT}`);
+})
