5ed09795ab11a513f364e292667d5d2c40102215
SilvenLEAF <SilvenLEAF@gmail.com>
2020-09-03 01:27:23 +0530

    1)create directory 2)remove directory 3)create file 4)read file 5)remove file-----with NODEJS

--- PlayingWithExpressAndNode/app.js
+++ PlayingWithExpressAndNode/app.js
@@ -1,2 +1,54 @@
-console.log(__dirname)
-console.log(__filename)
+const fs = require('fs');
+
+
+/* -------------------------------
+.         1)CREATE directory
+.         2)REMOVE directory
+.         3)CREATE file
+.         4)READ file
+.         5)REMOVE file
+------------------------------- */
+
+
+//-------------CREATING DIRECTORY
+fs.mkdir('NEW DIRECTORY', (err)=>{
+  if(err) throw err;
+  console.log(`New directory created!!`);    
+})
+
+
+
+
+//-------------REMOVING DIRECTORY
+fs.rmdir('NEW DIRECTORY', (err)=>{
+  if(err) throw err;
+  console.log(`New directory removed!!`);    
+})
+
+
+
+
+//-------------CREATING FILE
+fs.writeFile('NEW FILE.js', '', (err)=>{
+  if(err) throw err;
+  console.log(`New file created!!`);    
+})
+
+
+
+
+
+//-------------READ FILE
+fs.readFile('NEW FILE.js', 'utf8', (err, data)=>{
+  if(err) throw err;
+  console.log(data);    
+})
+
+
+
+
+//-------------REMOVE FILE
+fs.unlink('NEW FILE.js', (err)=>{
+  if(err) throw err;
+  console.log(`New file deleted!!`);    
+})
