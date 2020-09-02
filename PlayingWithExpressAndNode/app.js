989c7ec87079024bb78347f1c9bdc444eb56ba53
SilvenLEAF <SilvenLEAF@gmail.com>
2020-09-03 01:11:48 +0530

    getting dirname and filename with nodejs----------1)__dirname gives the absolute path of the directory with its name. 2)__filename gives us the absolute path of the file with its name. CONCLUSION: Both are same, just the __filename adds the filename to the __dirname result.

--- PlayingWithExpressAndNode/app.js
+++ PlayingWithExpressAndNode/app.js
@@ -1,26 +1,2 @@
-const express = require('express');
-
-
-// firng express app
-const app = express();
-app.use(express.json());
-
-
-
-/* ---------------------------------
-.            routes
---------------------------------- */
-
-
-
-
-
-
-
-
-
-// -------------------------------------LISTEN
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=>{
-  console.log(`Server is running on port${PORT}`);
-});
+console.log(__dirname)
+console.log(__filename)
