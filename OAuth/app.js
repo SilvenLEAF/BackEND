71d803b7a3611474c7f2b6ecf6d019d2f51cd57a
Manash Sarma <SilvenLEAF@gmail.com>
2020-09-09 06:49:52 +0530

    Added config

--- OAuth/app.js
+++ OAuth/app.js
@@ -1,3 +1,4 @@
+//required modules
 const express = require('express');
 
 
@@ -10,7 +11,7 @@ app.use(express.json());
 
 
 /* --------------------------------
-.           config
+.              config
 -------------------------------- */
 
 require('./config/passportSetup');
