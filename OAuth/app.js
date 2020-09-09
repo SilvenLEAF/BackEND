13176242659987a80fffe01d04330b4ec9a8d20d
Manash Sarma <SilvenLEAF@gmail.com>
2020-09-09 06:48:40 +0530

    Added confiv

--- OAuth/app.js
+++ OAuth/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
-const passportSetup = require('./config/passportSetup');
-
 
 
 // firing express app
 const app = express();
 app.use(express.json());
+
+
+
+
+
+/* --------------------------------
+.           config
+-------------------------------- */
+
+require('./config/passportSetup');
+
 
 
 
