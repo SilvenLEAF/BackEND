280d33b445949bcc2f8b498b764c0498a3135faa
Manash Sarma <SilvenLEAF@gmail.com>
2020-09-13 07:31:37 +0530

    Replaced local port with PRODUCTION PORT

--- REST-API/app.js
+++ REST-API/app.js
@@ -29,6 +29,7 @@ app.use((err, req, res, next)=>{
 /* --------------------
 .        Listen 
 -------------------- */
-app.listen(3000, ()=>{
-    console.log('Now listening for requests from the port 3000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT , ()=>{
+    console.log(´Now listening for requests from the port &{ PORT }´);
 });
